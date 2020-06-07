95aacfe66486ff0c54876f18b3e7122ac205dbc4
nada-265 <nodykhaled240@gmail.com>
2020-06-07 03:20:47 +0200

    all programs

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'programs',
-    'prog_app',
     'dss_box',
 ]
 
--- project/urls.py
+++ project/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('programs.urls')),
     path('', include('dss_box.urls')),
 
 
