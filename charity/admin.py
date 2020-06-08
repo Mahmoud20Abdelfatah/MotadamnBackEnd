beecbf54bd4d38064c943fe296defa4c2a5e1e45
Mahmoud20Abdelfatah <mahmoudmahmoud1511@gmail.com>
2020-06-08 21:34:34 +0200

    Clean code

--- charity/admin.py
+++ charity/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-
 from .models import Charity
-
+                                                 #importing
 
 admin.site.register(Charity)
+                                                 #to view charity in admin
--- supercategory/admin.py
+++ supercategory/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-
 from .models import Supercategory
-
+                                                 #importing
 
 admin.site.register(Supercategory)
+                                                 #to view donor in admin
+
